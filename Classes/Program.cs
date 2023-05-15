031a481771f041df87dbdab4d6551edc979aa393
ClPhillips10 <Lakephillips3232@gmail.com>
2023-05-15 18:29:46 +0100

    exercise 1

--- Classes/Program.cs
+++ Classes/Program.cs
@@ -4,7 +4,24 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var mycar = new Car();
+            mycar.Year = 2020;
+            mycar.Make = "Toyota";
+            mycar.Model = "Corolla";
+
+            var Worktruck = new Car()
+            {
+                Year = 2023,
+                Model = "Tundra",
+                Make = "Toyota",
+            };
+            
+
+            Console.WriteLine($" I drive a {mycar.Year} {mycar.Make} {mycar.Model}. When I am working I drive a {Worktruck.Year} {Worktruck.Make} {Worktruck.Model}.");
+
+
         }
-    }
+
+        }
 }
+
